d9bff676e1daf6a2efe097b79319778f77de2dca
Ramandeep <ramandeep8421@gmail.com>
2021-03-20 12:39:34 +0530

    initial commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from django.contrib import admin
-from .views import HomePage , BlogDetailView
+from .views import HomePage , BlogDetailView, BlogCreateView, BlogUpdateView , BlogDeleteView
 
 urlpatterns = [
     path('' , HomePage.as_view() , name='home'),
-    path('post/<int:pk>/' , BlogDetailView.as_view() , name='post_detail')
+    path('post/<int:pk>/' , BlogDetailView.as_view() , name='post_detail'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete', BlogDeleteView.as_view() , name = 'post_delete'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView , DetailView
+from django.views.generic import ListView , DetailView , CreateView ,UpdateView,DeleteView
 from .models import Post
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
+
+
 
 class HomePage(ListView):
     model = Post
@@ -13,5 +20,39 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
 
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_create.html"
+    fields = "__all__"
+
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ['title', 'body']
+
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url =  reverse_lazy('home')
+
+
+
+class BlogDeleteView(LoginRequiredMixin, DeleteView):
+    login_url = 'login'
+    redirect_field_name = "accounts/login"
+
+
+class BlogCreateView(LoginRequiredMixin, CreateView):
+    login_url = 'login'
+    redirect_field_name = "accounts/login"
+
+class BlogUpdateView(LoginRequiredMixin, CreateView):
+    login_url = 'login'
+    redirect_field_name = "accounts/login"
 
 
